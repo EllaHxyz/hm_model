aae1ad9543c29f9040cedd2df14be5cdc918f92b
Ella <fhu62@gatech.edu>
2022-12-09 13:01:24 -0500

    update align_bert

--- __init__.py
+++ __init__.py
@@ -2,3 +2,4 @@ import hm_model.processors.processors
 #import hm_model.models.concat_vl
 #import hm_model.models.concat_bert_tutorial
 import hm_model.models.concat_bert
+import hm_model.models.align_bert
