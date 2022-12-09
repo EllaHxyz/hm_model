840e4bbd4fd9393b575b9baab26a020c25f4a9a2
Ella <fhu62@gatech.edu>
2022-12-09 15:47:52 -0500

    add cross_bert model

--- __init__.py
+++ __init__.py
@@ -3,3 +3,5 @@ import hm_model.processors.processors
 #import hm_model.models.concat_bert_tutorial
 import hm_model.models.concat_bert
 import hm_model.models.align_bert
+import hm_model.models.cross_bert
+
